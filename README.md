ee6250c1a8b0527a71eb821a9ee3a5cf369acc1b
agepoor <agespoortinga@gmail.com>
2018-03-10 22:07:04 +0100

    added npm scripts for development

--- README.md
+++
@@ -1,1 +0,0 @@
-# Workflowy Theme Generator
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-safe-important'),
+    require('postcss-cssnext'),
+  ]
+}
